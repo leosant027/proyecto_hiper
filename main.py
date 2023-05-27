3c20742945fc3c98b4f4f4430deea2b619940645
Gabriel Santos <leosant027@gmail.com>
2023-05-27 17:03:05 -0300

    agregando main

---
+++ main.py
@@ -0,0 +1,16 @@
+import asyncio
+
+import requests
+import json
+
+from funciones.categoria import listar_categoria
+from funciones.filtros import selecionar_filtro
+
+
+url = "https://www.hiperlibertad.com.ar/api/catalog_system/pub/category/tree/50"
+response = requests.get(url)
+listar_categoria(response)
+filtro=selecionar_filtro()
+print(filtro.valor)
+
+
