c208e5982eec9b8b9d354b96d1ce0b684c617851
Gabriel Santos <leosant027@gmail.com>
2023-05-28 14:58:00 -0300

    configuando la funcion para realizar intentos y en caso de fallo esperar para reintentar

---
+++ funciones/api.py
@@ -0,0 +1,31 @@
+import time
+
+import requests
+
+
+def consumir_api(url, headers, max_reintentos=2):
+    """
+    Esta función envía una solicitud GET a una URL y devuelve la respuesta en formato JSON.
+
+    Parámetros:
+    url (str): La URL a la que se enviará la solicitud GET.
+    headers (dict): Un diccionario que contiene los encabezados HTTP de la solicitud.
+    max_reintentos (int, opcional): El número máximo de intentos que se harán si la solicitud falla. Por defecto es 2.
+
+    Retorna:
+    dict: La respuesta de la solicitud en formato JSON.
+
+    Lanza:
+    SystemExit: Si la solicitud falla después de max_reintentos, se lanza una excepción de tipo SystemExit.
+    """
+    for i in range(max_reintentos):
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()  # Lanza una excepción si la respuesta tiene un código de estado HTTP 4XX o 5XX
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            if i < max_reintentos - 1:  # si no es el último intento
+                time.sleep(5 * (i + 1))  # esperar 5 segundos en el primer intento, 10 en el segundo, etc.
+                continue
+            else:  # si es el último intento, lanzar la excepción
+                raise SystemExit(e)
