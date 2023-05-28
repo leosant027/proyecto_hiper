d376384f20083efaeceb3691f5b7efdcc20991da
Gabriel Santos <leosant027@gmail.com>
2023-05-28 20:58:15 -0300

    creo la clase categoria_temp para prepara el  categoria.json

---
+++ modelos/categoria_temp.py
@@ -0,0 +1,7 @@
+class CategoriaTemp:
+    def __init__(self, category_id, name, has_children, url, children):
+        self.category_id = category_id
+        self.name = name
+        self.has_children = has_children
+        self.url = url
+        self.children = children
