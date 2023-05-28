c1d1e606c54d1a13f36cf5cf069aa8ad546d8262
Gabriel Santos <leosant027@gmail.com>
2023-05-28 15:09:57 -0300

    creo el modelo de producto pata poder almacenar los datos

---
+++ modelos/producto.py
@@ -0,0 +1,10 @@
+class Producto:
+    def __init__(self, nombre,marca,categoria, precio,precio_lista,url,stock,sku):
+        self.nombre = nombre
+        self.marca = marca
+        self.categoria=categoria
+        self.precio=precio
+        self.precio_lista=precio_lista
+        self.url=url
+        self.stock=stock
+        self.sku = sku
