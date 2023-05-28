87d3afcc7dc288e04dfe84a5b8a038c5a6fe8b34
Gabriel Santos <leosant027@gmail.com>
2023-05-28 15:04:53 -0300

    creo un archivo para almacenar las constantes globales

---
+++ costante_gral.py
@@ -0,0 +1,4 @@
+# Este archivo define las constantes globales utilizadas en el programa.
+
+# URL_BASE es la URL base de la API a la que estamos realizando las solicitudes.
+URL_BASE="https://www.hiperlibertad.com.ar"
