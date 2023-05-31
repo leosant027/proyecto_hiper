5376e9562afa86198236fb404263e9dd5ad81655
Gabriel Santos <leosant027@gmail.com>
2023-05-30 21:36:35 -0300

    agregando progresbar al mapeo de categoria

--- funciones/categoria.py
+++ funciones/categoria.py
@@ -22,7 +22,7 @@ def listar_categoria(headers):
     url = f"{URL_BASE}/api/catalog_system/pub/category/tree/50"
     data = consumir_api(url, headers)
     categorias = []
-    categorias_y_subcategorias=[]
+    categorias_y_subcategorias = []
 
     # Parsear los datos de las categorías
     for category_data in data:
@@ -30,8 +30,8 @@ def listar_categoria(headers):
     for categoria in categorias:
         urlinfo = f"{URL_BASE}/api/catalog_system/pub/facets/search{categoria.url}/?map=c"
         data_info = consumir_api(urlinfo, headers)
-        paginas=[]
-        marcas=[]
+        paginas = []
+        marcas = []
         cantidad_productos = data_info['CategoriesTrees'][0]['Quantity']
         categoria.cantidad_productos = cantidad_productos
         # Creando paginacion
@@ -41,26 +41,26 @@ def listar_categoria(headers):
             fin = inicio + MAX_PRODUCTOS_POR_PAGINA
             pagina_dict = dict(pagina=pagina + 1, url=f"_from={inicio}&_to={fin}")
             paginas.append(pagina_dict)
-            categoria.paginas=paginas
+            categoria.paginas = paginas
         for marca in data_info['Brands']:
             marca_dict = dict(id=0, nombre=marca['Name'])
             marcas.append(marca_dict)
-            categoria.marcas=marcas
-        print("categoria "+categoria.name)
+            categoria.marcas = marcas
+        print("categoria " + categoria.name)
         print(f"cant productos {cantidad_productos}")
         print(categoria.paginas)
         for subcategoria in categoria.children:
             urlinfosubcat = f"{URL_BASE}/api/catalog_system/pub/facets/search{subcategoria.url}/?map=c,c"
             data_infosubcat = consumir_api(urlinfosubcat, headers)
             subcategoria.cantidad_productos = data_infosubcat['CategoriesTrees'][0]['Quantity']
-            print("sub categoria "+subcategoria.name)
+            print("sub categoria " + subcategoria.name)
             print(subcategoria.cantidad_productos)
             for subcategoria_2 in subcategoria.children:
                 urlinfosubcat_2 = f"{URL_BASE}/api/catalog_system/pub/facets/search{subcategoria.url}/?map=c,c"
                 data_infosubcat_2 = consumir_api(urlinfosubcat_2, headers)
                 subcategoria_2.cantidad_productos = data_infosubcat_2['CategoriesTrees'][0]['Quantity']
-                print("sub categoria 2 "+subcategoria_2.name)
-        categorias_y_subcategorias.append(categorias)
+                print("sub categoria 2 " + subcategoria_2.name)
+    categorias_y_subcategorias.append(categorias)
 
     # Guardar los datos parseados en un archivo JSON
     with open('data/categorias.json', 'w', encoding='utf-8') as f:
@@ -79,20 +79,35 @@ def mapear_categorias():
     # Crear un nuevo JSON
     nuevo_json = []
 
-    # Procesar cada elemento en el JSON proporcionado
-    for item in data:
+    # Configurar el progreso bar
+    total_items = len(data[0])
+    progress_bar = tqdm(total=total_items, desc="Mapeando Categorías", unit="item")
+
+    # Procesar cada item en el JSON proporcionado
+    for item in data[0]:
         # Añadir la categoría principal
         nuevo_json.append({
-            "id": item["categoria_id"],
-            "nombre": item["nombre"]
+            "id": item["category_id"],
+            "nombre": item["name"]
         })
 
         # Añadir cada subcategoría
-        for subcategoria in item.get("subcategorias", []):
+        for subcategoria in item.get("children", []):
             nuevo_json.append({
-                "id": subcategoria["categoria_id"],
-                "nombre": subcategoria["nombre"]
+                "id": subcategoria["category_id"],
+                "nombre": subcategoria["name"]
             })
+
+            # Añadir cada subcategoría 2
+            for subcategoria_2 in subcategoria.get("children", []):
+                nuevo_json.append({
+                    "id": subcategoria_2["category_id"],
+                    "nombre": subcategoria_2["name"]
+                })
+
+        progress_bar.update(1)
+
+    progress_bar.close()
 
     # Ordenar la lista resultante por id
     nuevo_json = sorted(nuevo_json, key=lambda x: x['id'])
@@ -101,15 +116,15 @@ def mapear_categorias():
 
 def actualizar_categorias(archivo_csv):
     categorias = leer_json(f'{RUTA_DATOS}lista_categoria.json')
-   # print(categorias)
+    # print(categorias)
     filas, nombres_campos = leer_csv(archivo_csv)
     # filas = filas[1:]
     filas_actualizadas = []
 
     for fila in filas:
-       # print(fila)
-        #print(nombres_campos)
-        categoria_id = int(obtener_valor(fila,'categoria_id'))
+        # print(fila)
+        # print(nombres_campos)
+        categoria_id = int(obtener_valor(fila, 'categoria_id'))
         for categoria in categorias:
             if categoria_id == int(categoria['id']):
                 fila['categoria'] = categoria['nombre']
