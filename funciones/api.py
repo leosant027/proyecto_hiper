e855e122d97615d6e2aeff04a59c96b986822f8b
Gabriel Santos <leosant027@gmail.com>
2023-05-31 18:35:30 -0300

    Agrego soporte para proxy y actulizo la documentación

--- funciones/api.py
+++ funciones/api.py
@@ -5,14 +5,14 @@ import requests
 from costante_gral import MAX_REINTENTOS
 
 
-def consumir_api(url, headers):
+def consumir_api(url, headers, proxy=None):
     """
     Esta función envía una solicitud GET a una URL y devuelve la respuesta en formato JSON.
 
     Parámetros:
     url (str): La URL a la que se enviará la solicitud GET.
     headers (dict): Un diccionario que contiene los encabezados HTTP de la solicitud.
-    max_reintentos (int, opcional): El número máximo de intentos que se harán si la solicitud falla. Por defecto es 2.
+    proxy (str, opcional): La dirección del proxy a utilizar. Por defecto es None.
 
     Retorna:
     dict: La respuesta de la solicitud en formato JSON.
@@ -23,7 +23,15 @@ def consumir_api(url, headers):
     max_reintentos = MAX_REINTENTOS
     for i in range(max_reintentos):
         try:
-            response = requests.get(url, headers=headers)
+            if proxy:
+                proxies = {
+                    'http': proxy,
+                    'https': proxy
+                }
+                response = requests.get(url, headers=headers, proxies=proxies)
+            else:
+                response = requests.get(url, headers=headers)
+
             response.raise_for_status()  # Lanza una excepción si la respuesta tiene un código de estado HTTP 4XX o 5XX
             return response.json()
         except requests.exceptions.RequestException as e:
